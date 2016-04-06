7a3b56ebca8e0d52084987aa5e7fd15112847d0b
Tate Allen <tatea7@yahoo.com>
2016-04-05 18:31:52 -0700

    Initial Commit

---
+++ ModularSoftSynth/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ModularSoftSynth
+//
+//  Created by Tate Allen on 4/5/16.
+//  Copyright © 2016 tate. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
